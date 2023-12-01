0123be7314bcebda501bf9e23a0882464a71f355
Mauro <mauro.bobyk@gmail.com>
2023-12-01 08:24:28 -0300

    agregado Models

--- app/Models/Medico.php
+++ app/Models/Medico.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Medico extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'matricula',
+        'especialidad',
+        'fecha_ingreso',
+    ];
 }
